4ab2bc28ebaaecdbf06486bd2e16e2f5f5353dab
fleckm210 <fleckm210@gmail.com>
2023-01-02 12:21:36 -0700

    added unity project to repo

---
+++ complete project/Assets/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //getting variables for basic movement
+    public Rigidbody rb;
+    public Transform tf;
+    float movement = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //uses arrow keys input for movement
+    void Update()
+    {
+        //gets input from arrow keys
+        if (Input.GetKey(KeyCode.A))
+        {
+            //moves non-kenematic object in specified direction
+            rb.MovePosition(new Vector3(tf.position.x - movement, tf.position.y, tf.position.z));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(new Vector3(tf.position.x + movement, tf.position.y, tf.position.z));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(new Vector3(tf.position.x, tf.position.y, tf.position.z + movement));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(new Vector3(tf.position.x, tf.position.y, tf.position.z - movement));
+        }
+    }
+}
---
+++ complete project/Assets/zombie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class zombie : MonoBehaviour
+{
+    //gets pathfinding agent of the object
+    public NavMeshAgent nav;
+
+    //gets player transform for pathfinding
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assigns local pointer to player object
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets pathfinding destination to players transform
+        nav.destination = player.transform.position;
+    }
+}
